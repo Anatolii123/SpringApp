8f409366d1e42d8b142b2ebeae997b1a7d94c1e7
AKusakin <anatolii.kusakin1234@gmail.com>
2019-10-25 11:38:12 +0500

    Hibernate2Commit

---
+++ src/main/java/application/entity/People.java
@@ -0,0 +1,144 @@
+package application.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Time;
+
+@Entity
+public class People {
+    private long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Time dateOfBirth;
+    private String gender;
+    private String bug;
+    private String comments;
+
+    @Id
+    @Column(name = "ID", nullable = false, precision = 0)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "NAME", nullable = true, length = 45)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "SURNAME", nullable = true, length = 45)
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Basic
+    @Column(name = "EMAIL", nullable = true, length = 128)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "PASSWORD", nullable = true, length = 128)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "DATE_OF_BIRTH", nullable = true)
+    public Time getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Time dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Basic
+    @Column(name = "GENDER", nullable = false, length = 45)
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Basic
+    @Column(name = "BUG", nullable = false, length = 45)
+    public String getBug() {
+        return bug;
+    }
+
+    public void setBug(String bug) {
+        this.bug = bug;
+    }
+
+    @Basic
+    @Column(name = "COMMENTS", nullable = true, length = 45)
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        People people = (People) o;
+
+        if (id != people.id) return false;
+        if (name != null ? !name.equals(people.name) : people.name != null) return false;
+        if (surname != null ? !surname.equals(people.surname) : people.surname != null) return false;
+        if (email != null ? !email.equals(people.email) : people.email != null) return false;
+        if (password != null ? !password.equals(people.password) : people.password != null) return false;
+        if (dateOfBirth != null ? !dateOfBirth.equals(people.dateOfBirth) : people.dateOfBirth != null) return false;
+        if (gender != null ? !gender.equals(people.gender) : people.gender != null) return false;
+        if (bug != null ? !bug.equals(people.bug) : people.bug != null) return false;
+        if (comments != null ? !comments.equals(people.comments) : people.comments != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (dateOfBirth != null ? dateOfBirth.hashCode() : 0);
+        result = 31 * result + (gender != null ? gender.hashCode() : 0);
+        result = 31 * result + (bug != null ? bug.hashCode() : 0);
+        result = 31 * result + (comments != null ? comments.hashCode() : 0);
+        return result;
+    }
+}
