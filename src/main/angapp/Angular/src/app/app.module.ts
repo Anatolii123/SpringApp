b26b1057623de9ec48aa226f61a0c05e0de3c736
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-06 17:44:31 +0500

    AngularApp4Commit

--- src/main/angapp/Angular/src/app/app.module.ts
+++ src/main/angapp/Angular/src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { KontaktiComponent } from './components/kontakti/kontakti.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: '', component:LogInComponent},
+  {path: 'about', component:KontaktiComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    KontaktiComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
