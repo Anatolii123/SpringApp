7c1a46cf4cd07d368147ed100d6faf3c4de182ef
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-12 20:02:34 +0500

    AngularApp11Commit

--- src/main/java/application/controller/UserRestController.java
+++ src/main/java/application/controller/UserRestController.java
@@ -8,12 +8,25 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/")
 public class UserRestController {
 
+    public static class SaltResponse{
+        private String salt;
+
+        public String getSalt() {
+            return salt;
+        }
+
+        public void setSalt(String salt) {
+            this.salt = salt;
+        }
+    }
+
     @CrossOrigin(value = "*")
     @GetMapping("/Salt")
-    public String getSalt(HttpSession session) {
-        String salt = Long.toHexString((long) ((Math.random() * 900000000000000000L) + 100000000000000000L));
-        session.setAttribute("salt", salt);
-        return salt;
+    public SaltResponse getSalt(HttpSession session) {
+        SaltResponse response = new SaltResponse();
+        response.setSalt(Long.toHexString((long) ((Math.random() * 900000000000000000L) + 100000000000000000L)));
+        session.setAttribute("salt", response.getSalt());
+        return response;
     }
 
 }
