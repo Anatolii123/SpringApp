4e1aa16cccd3dc5a9ea959d3b6fc5601ad4f1185
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-20 18:56:05 +0500

    AngularApp83Commit

--- src/main/java/application/controller/MatrixCalculationController.java
+++ src/main/java/application/controller/MatrixCalculationController.java
@@ -1,11 +1,45 @@
 package application.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import application.factory.Matrix;
+import application.factory.MatrixMultiplicator;
+import application.factory.MatrixSubstractor;
+import application.factory.MatrixSummator;
+import org.springframework.web.bind.annotation.*;
+import application.MatrixCalc;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(value = "*")
-@RequestMapping("/")
+@RequestMapping("/Calc")
 public class MatrixCalculationController {
+
+    @PostMapping(value = "/Add", params = {"matrix1","matrix2"})
+    public List<Matrix> sumMatrices(@RequestParam("matrix1") List<Matrix> matrix1, @RequestParam("matrix2") List<Matrix> matrix2,
+                                HttpSession session) {
+        List<Matrix> result = new ArrayList<>();
+        MatrixCalc matrixCalc = new MatrixCalc();
+        matrixCalc.performOperation(session, new MatrixSummator(), matrix1, matrix2);
+        return result;
+    }
+
+    @PostMapping(value = "/Subsctract", params = {"matrix1","matrix2"})
+    public List<Matrix> subMatrices(@RequestParam("matrix1") List<Matrix> matrix1, @RequestParam("matrix2") List<Matrix> matrix2,
+                                    HttpSession session) {
+        List<Matrix> result = new ArrayList<>();
+        MatrixCalc matrixCalc = new MatrixCalc();
+        matrixCalc.performOperation(session, new MatrixSubstractor(), matrix1, matrix2);
+        return result;
+    }
+
+    @PostMapping(value = "/Multiplicate", params = {"matrix1","matrix2"})
+    public List<Matrix> multMatrices(@RequestParam("matrix1") List<Matrix> matrix1, @RequestParam("matrix2") List<Matrix> matrix2,
+                                    HttpSession session) {
+        List<Matrix> result = new ArrayList<>();
+        MatrixCalc matrixCalc = new MatrixCalc();
+        matrixCalc.performOperation(session, new MatrixMultiplicator(), matrix1, matrix2);
+        return result;
+    }
 }
