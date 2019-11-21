d54c35c103e93f766094c0bcfc2eafccd6852451
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-21 16:32:11 +0500

    AngularApp91Commit

--- src/main/java/application/controller/MatrixCalculationController.java
+++ src/main/java/application/controller/MatrixCalculationController.java
@@ -31,7 +31,7 @@ public class MatrixCalculationController {
         return result;
     }
 
-    @PostMapping(value = "/Subsctract")
+    @PostMapping(value = "/Substract")
     public Long[][] subMatrices(@RequestBody CalcRequest request, HttpSession session) {
         List<Matrix> matrix1 = new ArrayList<>();
         matrix1.add(request.getMatrix1());
@@ -47,14 +47,14 @@ public class MatrixCalculationController {
         return result;
     }
 
-    @PostMapping(value = "/Multiplicate")
+    @PostMapping(value = "/Multiply")
     public Long[][] multMatrices(@RequestBody CalcRequest request, HttpSession session) {
         List<Matrix> matrix1 = new ArrayList<>();
         matrix1.add(request.getMatrix1());
         List<Matrix> matrix2 = new ArrayList<>();
         matrix2.add(request.getMatrix2());
         Long[][] result = new Long[request.getMatrix1().getA()][request.getMatrix2().getB()];
-        new MatrixCalc().performOperation(session, new MatrixSubstractor(), matrix1, matrix2);
+        new MatrixCalc().performOperation(session, new MatrixMultiplicator(), matrix1, matrix2);
         for (int i = 1; i <= request.getMatrix1().getA(); i++) {
             for (int j = 1; j <= request.getMatrix2().getB(); j++) {
                 result[i-1][j-1] = Long.parseLong(session.getAttribute("m3" + i + j).toString());
