ed3027b3c0206955273b373746899f11876b3653
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-21 16:23:53 +0500

    AngularApp89Commit

--- src/main/java/application/controller/MatrixCalculationController.java
+++ src/main/java/application/controller/MatrixCalculationController.java
@@ -1,9 +1,6 @@
 package application.controller;
 
-import application.factory.Matrix;
-import application.factory.MatrixMultiplicator;
-import application.factory.MatrixSubstractor;
-import application.factory.MatrixSummator;
+import application.factory.*;
 import org.springframework.web.bind.annotation.*;
 import application.MatrixCalc;
 
@@ -17,10 +14,21 @@ import java.util.List;
 public class MatrixCalculationController {
 
     @PostMapping(value = "/Add")
-    public List<Matrix> sumMatrices(@RequestBody CalcRequest request, HttpSession session) {
-        List<Matrix> result = new ArrayList<>();
-        MatrixCalc matrixCalc = new MatrixCalc();
-        //matrixCalc.performOperation(session, new MatrixSummator(), request.getMatrix1(), request.getMatrix2());
+    public Long[][] sumMatrices(@RequestBody CalcRequest request, HttpSession session) {
+        List<Matrix> matrix1 = new ArrayList<>();
+        matrix1.add(request.getMatrix1());
+        List<Matrix> matrix2 = new ArrayList<>();
+        matrix2.add(request.getMatrix2());
+        Long[][] result = new Long[request.getMatrix1().getA()][request.getMatrix2().getB()];
+        new MatrixCalc().performOperation(session, new MatrixSummator(), matrix1, matrix2);
+        Operations[][] res = new Operations[request.getMatrix1().getA()][request.getMatrix2().getB()];
+        for (int i = 1; i <= request.getMatrix1().getA(); i++) {
+            for (int j = 1; j <= request.getMatrix2().getB(); j++) {
+               result[i-1][j-1] = Long.parseLong(session.getAttribute("m3" + i + j).toString());
+            }
+        }
+        Matrix resultMatrix = new Matrix();
+        resultMatrix.setMatrix(res);
         return result;
     }
 
