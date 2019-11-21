e4096a03b4591c0ced0fce2526d103be4e2c65eb
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-21 16:38:15 +0500

    AngularApp92Commit

--- src/main/java/application/controller/MatrixCalculationController.java
+++ src/main/java/application/controller/MatrixCalculationController.java
@@ -14,52 +14,34 @@ import java.util.List;
 public class MatrixCalculationController {
 
 
-
-    @PostMapping(value = "/Add")
-    public Long[][] sumMatrices(@RequestBody CalcRequest request, HttpSession session) {
+    public Long[][] calcMatrices(CalcRequest request, HttpSession session, MatrixOperation matrixOperation) {
         List<Matrix> matrix1 = new ArrayList<>();
         matrix1.add(request.getMatrix1());
         List<Matrix> matrix2 = new ArrayList<>();
         matrix2.add(request.getMatrix2());
         Long[][] result = new Long[request.getMatrix1().getA()][request.getMatrix2().getB()];
-        new MatrixCalc().performOperation(session, new MatrixSummator(), matrix1, matrix2);
+        new MatrixCalc().performOperation(session, matrixOperation, matrix1, matrix2);
         for (int i = 1; i <= request.getMatrix1().getA(); i++) {
             for (int j = 1; j <= request.getMatrix2().getB(); j++) {
-               result[i-1][j-1] = Long.parseLong(session.getAttribute("m3" + i + j).toString());
+                result[i-1][j-1] = Long.parseLong(session.getAttribute("m3" + i + j).toString());
             }
         }
         return result;
+    }
+
+
+    @PostMapping(value = "/Add")
+    public Long[][] sumMatrices(@RequestBody CalcRequest request, HttpSession session) {
+        return calcMatrices(request,session,new MatrixSummator());
     }
 
     @PostMapping(value = "/Substract")
     public Long[][] subMatrices(@RequestBody CalcRequest request, HttpSession session) {
-        List<Matrix> matrix1 = new ArrayList<>();
-        matrix1.add(request.getMatrix1());
-        List<Matrix> matrix2 = new ArrayList<>();
-        matrix2.add(request.getMatrix2());
-        Long[][] result = new Long[request.getMatrix1().getA()][request.getMatrix2().getB()];
-        new MatrixCalc().performOperation(session, new MatrixSubstractor(), matrix1, matrix2);
-        for (int i = 1; i <= request.getMatrix1().getA(); i++) {
-            for (int j = 1; j <= request.getMatrix2().getB(); j++) {
-                result[i-1][j-1] = Long.parseLong(session.getAttribute("m3" + i + j).toString());
-            }
-        }
-        return result;
+        return calcMatrices(request,session,new MatrixSubstractor());
     }
 
     @PostMapping(value = "/Multiply")
     public Long[][] multMatrices(@RequestBody CalcRequest request, HttpSession session) {
-        List<Matrix> matrix1 = new ArrayList<>();
-        matrix1.add(request.getMatrix1());
-        List<Matrix> matrix2 = new ArrayList<>();
-        matrix2.add(request.getMatrix2());
-        Long[][] result = new Long[request.getMatrix1().getA()][request.getMatrix2().getB()];
-        new MatrixCalc().performOperation(session, new MatrixMultiplicator(), matrix1, matrix2);
-        for (int i = 1; i <= request.getMatrix1().getA(); i++) {
-            for (int j = 1; j <= request.getMatrix2().getB(); j++) {
-                result[i-1][j-1] = Long.parseLong(session.getAttribute("m3" + i + j).toString());
-            }
-        }
-        return result;
+        return calcMatrices(request,session,new MatrixMultiplicator());
     }
 }
