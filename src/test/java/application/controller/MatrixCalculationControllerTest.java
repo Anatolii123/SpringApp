e6db6a6db72a5f7a6fc03dabf063bac527a083e4
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-22 16:01:18 +0500

    AngularApp102Commit

---
+++ src/test/java/application/controller/MatrixCalculationControllerTest.java
@@ -0,0 +1,14 @@
+package application.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MatrixCalculationController.class)
+public class MatrixCalculationControllerTest {
+    @Test
+    public void test() throws Exception {
+    }
+}
