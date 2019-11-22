54c07b7e669ad7c80092cd42cb1b7baf735efa39
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-22 18:37:18 +0500

    AngularApp105Commit

--- src/main/java/application/config/WebConfig.java
+++ src/main/java/application/config/WebConfig.java
@@ -1,16 +1,23 @@
 package application.config;
 
 import org.springframework.context.annotation.*;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
@@ -30,6 +37,25 @@ public class WebConfig extends WebMvcConfigurerAdapter implements WebMvcConfigur
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public ContentNegotiatingViewResolver contentViewResolver() {
+        ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+        contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
+        defaultView.setExtractValueFromSingleKeyModel(true);
+
+        ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+        contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+        contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(viewResolver));
+        contentViewResolver.setDefaultViews(Arrays.<View> asList(defaultView));
+        return contentViewResolver;
     }
 
     @Override
--- src/test/java/application/controller/MatrixCalculationControllerTest.java
+++ src/test/java/application/controller/MatrixCalculationControllerTest.java
@@ -5,24 +5,102 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.nio.charset.Charset;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MatrixCalculationController.class)
 @AutoConfigureMockMvc
+@EnableWebMvc
 public class MatrixCalculationControllerTest {
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(APPLICATION_JSON.getType(),
+            APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    String request = new String("{ \n" +
+            "   \"matrix1\":{ \n" +
+            "         \"a\":\"2\",\n" +
+            "         \"b\":\"2\",\n" +
+            "         \"matrix\":[ \n" +
+            "            [ \n" +
+            "               { \n" +
+            "                  \"value\" : 1\n" +
+            "               },\n" +
+            "               { \n" +
+            "                  \"value\" : 0\n" +
+            "               }\n" +
+            "            ],\n" +
+            "            [ \n" +
+            "               { \n" +
+            "                  \"value\" : 0\n" +
+            "               },\n" +
+            "               { \n" +
+            "                  \"value\" : 1\n" +
+            "               }\n" +
+            "            ]\n" +
+            "         ]\n" +
+            "   },\n" +
+            "   \"matrix2\":{ \n" +
+            "         \"a\":\"2\",\n" +
+            "         \"b\":\"2\",\n" +
+            "         \"matrix\":[ \n" +
+            "            [ \n" +
+            "               { \n" +
+            "                  \"value\" : 1\n" +
+            "               },\n" +
+            "               { \n" +
+            "                  \"value\" : 0\n" +
+            "               }\n" +
+            "            ],\n" +
+            "            [ \n" +
+            "               { \n" +
+            "                  \"value\" : 0\n" +
+            "               },\n" +
+            "               { \n" +
+            "                  \"value\" : 1\n" +
+            "               }\n" +
+            "            ]\n" +
+            "         ]\n" +
+            "   }\n" +
+            "}");
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private MatrixCalculationController controller;
+    @Test
+    public void testAddMethod() throws Exception {
+        this.mockMvc.perform(post("/Calc/Add").accept(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(request))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @Test
-    public void test() throws Exception {
-        assertThat(controller).isNotNull();
+    public void testSubstractMethod() throws Exception {
+        this.mockMvc.perform(post("/Calc/Substract").accept(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(request))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
+
+    @Test
+    public void testMultiplyMethod() throws Exception {
+        this.mockMvc.perform(post("/Calc/Multiply").accept(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(request))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
 }
