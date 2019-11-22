43a41d65f350753a0627c0a8d7395a3cbb2ce86c
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-22 16:05:37 +0500

    AngularApp103Commit

--- src/test/java/application/controller/MatrixCalculationControllerTest.java
+++ src/test/java/application/controller/MatrixCalculationControllerTest.java
@@ -1,14 +1,23 @@
 package application.controller;
 
+import application.controller.MatrixCalculationController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MatrixCalculationController.class)
 public class MatrixCalculationControllerTest {
+
+    @Autowired
+    private MatrixCalculationController controller;
+
     @Test
     public void test() throws Exception {
+        assertThat(controller).isNotNull();
     }
 }
