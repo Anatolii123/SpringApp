a5884758ab307e9aae4c70ae0d44d55786c43201
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-25 17:28:42 +0500

    AngularApp109Commit

--- src/main/java/application/controller/MatrixCalculationController.java
+++ src/main/java/application/controller/MatrixCalculationController.java
@@ -1,28 +1,32 @@
 package application.controller;
 
-import application.exceptions.DifferentSizesException;
-import application.exceptions.NotConsistentException;
+import application.MatrixCalc;
 import application.factory.*;
 import org.springframework.web.bind.annotation.*;
-import application.MatrixCalc;
 
 @RestController
 @CrossOrigin(value = "*")
 @RequestMapping("/Calc")
 public class MatrixCalculationController {
 
-
     public Long[][] calcMatrices(CalcRequest request, MatrixOperationWithCheck matrixOperation) throws Exception {
         return new MatrixCalc().performOperation(matrixOperation, request.getMatrix1(), request.getMatrix2());
     }
 
+    CalcMatrices co2 = (m1) -> {
+        return calcMatrices(new CalcRequest(),m1);
+    };
+
+//    MatrixOperation mo2 = (m1, m2) -> {
+//        return new MatrixSubstractor().perform(m1,m2);
+//    };
+//    MatrixOperation mo3 = (m1, m2) -> {
+//        return new MatrixMultiplicator().perform(m1,m2);
+//    };
 
     @PostMapping(value = "/Add")
     public Long[][] sumMatrices(@RequestBody CalcRequest request) throws Exception {
-        MatrixOperation mo = (m1, m2) -> {
-            return null;
-        };
-        return calcMatrices(request, new MatrixSummator());
+        return co2.calcMatrices(new MatrixSummator());
     }
 
     @PostMapping(value = "/Substract")
---
+++ src/main/java/application/factory/CalcMatrices.java
@@ -0,0 +1,5 @@
+package application.factory;
+
+public interface CalcMatrices {
+    public Long[][] calcMatrices(MatrixOperationWithCheck matrixOperation) throws Exception;
+}
